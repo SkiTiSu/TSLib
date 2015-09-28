13ae8abda8a8ed50e44ffb2defd14ace3ee3d012
SkiTiSu <sjtsjjy@163.com>
2015-09-28 22:27:03 +0800

    Controls: 增加垂直进度条

---
+++ TSLib.Controls/VerticalProgressBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+
+namespace TSLib.Controls
+{
+    /// <summary>
+    /// 垂直进度条
+    /// </summary>
+    public partial class VerticalProgressBar : ProgressBar
+    {
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.Style |= 0x04;
+                return cp;
+            }
+        }
+
+    }
+}
